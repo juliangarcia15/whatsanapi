c04764d6babf211771e1c80f6ed93d5334926343
juliangarcia15 <julian@appdetex.com>
2016-08-14 23:01:31 -0700

    Navtabs broken out of main view; Replaced with Navbar (not hooked yet, just basic html)

--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,8 @@ define(function(require) {
     var Backbone            = require('Backbone');
     var Mustache            = require('Mustache');
     var HomeTmpl            = require('text!templates/homeView.tmpl');
+    var NavtabsTmpl         = require('text!templates/navtabs.tmpl');
+    var NavbarTmpl          = require('text!templates/navbarSearchForm.tmpl');
     var GifView             = require('scripts/views/giphyView.js');
     var ApodView            = require('scripts/views/apodView.js');
 
@@ -11,6 +13,10 @@ define(function(require) {
     var AppView = Backbone.View.extend({
         // el - stands for element. Every view has an element associated with HTML content, will be rendered.
         el: 'body',
+        template: HomeTmpl,
+        partials: {
+            'navigation': NavbarTmpl,//NavtabsTmpl,
+        },
 
         // It's the first function called when this view is instantiated.
         initialize: function(options) {
@@ -20,7 +26,7 @@ define(function(require) {
         },
         // $el - it's a cached jQuery object (el), in which you can use jQuery functions to push content. Like the Hello TutorialsPoint in this case.
         render: function() {
-            this.$el.find('#home-view').html(Mustache.to_html(HomeTmpl));
+            this.$el.find('#home-view').html(Mustache.to_html(this.template, {}, this.partials));
 
             var giphyView = new GifView();
             var apodView = new ApodView();
